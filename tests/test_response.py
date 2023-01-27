2397b522b3b27e90e483217d6e3df0818cd3963b
Pavel Perestoronin <eigenein@gmail.com>
2023-01-27 13:48:36 +0100

    NEW: `test_error_inheritance`

---
+++ tests/test_response.py
@@ -0,0 +1,12 @@
+from combadge.response import FaultyResponse
+
+
+def test_error_inheritance() -> None:
+    class Foo(FaultyResponse):
+        pass
+
+    class Bar(Foo):
+        pass
+
+    assert Bar.Error is not Foo.Error
+    assert issubclass(Bar.Error, Foo.Error)
