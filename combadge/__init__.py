ce175b36e12777e3d169f688e2bc1ed45834c937
Pavel Perestoronin <eigenein@gmail.com>
2023-02-01 19:34:15 +0100

    FIX: override `Protocol` for PyPy

--- combadge/__init__.py
+++ combadge/__init__.py
@@ -1,11 +1,13 @@
 import typing
 from platform import python_implementation
 
-import typing_extensions
-
 if python_implementation() == "PyPy":
     # MyPy doesn't support `ParamSpec` in `Protocol` 😮
     del typing.Protocol
+
+# It MUST be imported only after the `typing` attributes were deleted.
+# Otherwise, it would just pick up the broken ones.
+import typing_extensions
 
 if python_implementation() == "PyPy":
     typing.Protocol = typing_extensions.Protocol  # type: ignore[assignment]
