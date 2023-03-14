de692d945dafc6f56fb5cb8ad6929955f5150896
Pavel Perestoronin <eigenein@gmail.com>
2023-03-14 17:28:49 +0100

    OPT: add the docstring

--- combadge/support/shared/contextlib.py
+++ combadge/support/shared/contextlib.py
@@ -6,4 +6,5 @@ _T = TypeVar("_T")
 
 @asynccontextmanager
 async def asyncnullcontext(enter_result: _T) -> AsyncIterator[_T]:
+    """Compatibility with the older Python where `nullcontext` does not support async."""
     yield enter_result
