0fa8196f2cac16d9196d9ea549ede9c00faba455
Pavel Perestoronin <eigenein@gmail.com>
2023-03-14 17:14:36 +0100

    FIX: copy JSON fields and form fields in HTTP request

--- combadge/support/http/request.py
+++ combadge/support/http/request.py
@@ -10,14 +10,14 @@ class Request(RequiresMethod, RequiresPath, SupportsJson, SupportsQueryParams, S
 
     def to_json_dict(self) -> Dict[str, Any]:
         """Convert the JSON body and loose JSON fields into a dictionary."""
-        dict_ = self.json_fields
+        dict_ = self.json_fields.copy()
         if (json := self.json_) is not None:
             dict_.update(json.dict(by_alias=True))
         return dict_
 
     def to_form_data(self) -> Dict[str, List[Any]]:
         """Convert the form data and loose form fields into a dictionary."""
-        form_fields = self.form_fields
+        form_fields = self.form_fields.copy()
         if (form_data := self.form_data) is not None:
             for name, value in form_data.dict(by_alias=True).items():
                 self.append_form_field(name, value)
