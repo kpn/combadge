8b8b395fa9ee7ee1711ee13faff8044314de9166
Pavel Perestoronin <eigenein@gmail.com>
2023-10-31 21:46:52 +0100

    OPT: extract `combadge.core.BaseRequest`

--- combadge/core/request.py
+++ combadge/core/request.py
@@ -1,51 +1,57 @@
-from typing import Any, Iterable, Mapping, NoReturn, Type, Union
+from abc import ABC
+from typing import Any, Iterable, Mapping, NoReturn, Union
+
+from pydantic import BaseModel
+from typing_extensions import Self
 
 from combadge.core.binder import BaseBoundService
 from combadge.core.signature import Signature
-from combadge.core.typevars import RequestT
 
 
-def build_request(
-    request_class: Type[RequestT],
-    signature: Signature,
-    service: BaseBoundService,
-    call_args: Iterable[Any],
-    call_kwargs: Mapping[str, Any],
-) -> Union[RequestT, NoReturn]:
-    """
-    Build a request using the provided request type, marks, and service call arguments.
+class BaseRequest(ABC, BaseModel):
+    """Base class for protocol-dependent requests."""
 
-    Args:
-        request_class: target back-end request class
-        signature: extracted description of the protocol service method
-        service: an instance of a service class, on which the method is being called
-        call_args: service method call positional arguments
-        call_kwargs: service method call keyword arguments
-    """
+    @classmethod
+    def build_from(
+        cls,
+        signature: Signature,
+        service: BaseBoundService,
+        call_args: Iterable[Any],
+        call_kwargs: Mapping[str, Any],
+    ) -> Union[Self, NoReturn]:
+        """
+        Build a request using the provided request type, marks, and service call arguments.
 
-    bound_arguments = signature.bind_arguments(service, *call_args, **call_kwargs)
-    bound_arguments.apply_defaults()
-    all_arguments = bound_arguments.arguments
+        Args:
+            signature: extracted description of the protocol service method
+            service: an instance of a service class, on which the method is being called
+            call_args: service method call positional arguments
+            call_kwargs: service method call keyword arguments
+        """
 
-    # Construct an initial empty request without validation.
-    request = request_class.model_construct()
+        bound_arguments = signature.bind_arguments(service, *call_args, **call_kwargs)
+        bound_arguments.apply_defaults()
+        all_arguments = bound_arguments.arguments
 
-    # Apply the method markers: they receive all the arguments at once.
-    for marker in signature.method_markers:
-        marker.prepare_request(request, bound_arguments)
+        # Construct an initial empty request without validation.
+        request = cls.model_construct()
 
-    # Apply the parameter markers: they receive their respective values.
-    for marker in signature.parameter_descriptors:
-        try:
-            value = all_arguments[marker.name]
-        except KeyError:
-            # The parameter is not provided, skip the marker.
-            pass
-        else:
-            # Allow for lazy loaded default parameters.
-            if callable(value):
-                value = value()
-            marker.prepare_request(request, value)
+        # Apply the method markers: they receive all the arguments at once.
+        for marker in signature.method_markers:
+            marker.prepare_request(request, bound_arguments)
 
-    # Re-validate and return the request.
-    return request_class.model_validate(request, from_attributes=True)
+        # Apply the parameter markers: they receive their respective values.
+        for marker in signature.parameter_descriptors:
+            try:
+                value = all_arguments[marker.name]
+            except KeyError:
+                # The parameter is not provided, skip the marker.
+                pass
+            else:
+                # Allow for lazy loaded default parameters.
+                if callable(value):
+                    value = value()
+                marker.prepare_request(request, value)
+
+        # Re-validate and return the request.
+        return cls.model_validate(request, from_attributes=True)
