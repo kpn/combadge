dfbaa314e693f793b11253b2a1067f05f6aad4d2
Pavel Perestoronin <eigenein@gmail.com>
2023-11-13 15:03:35 +0100

    BREAK: remove the `request_with` functionality 💥
    It used to be a workaround in the early versions and is not needed now

--- combadge/support/shared/sync.py
+++
@@ -1,9 +0,0 @@
-from contextlib import AbstractContextManager
-from typing import Callable, Generic
-
-from combadge.core.typevars import BackendRequestT
-
-
-class SupportsRequestWith(Generic[BackendRequestT]):  # noqa: D101
-    def __init__(self, request_with: Callable[[BackendRequestT], AbstractContextManager]) -> None:  # noqa: D107
-        self._request_with = request_with
