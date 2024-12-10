49d96cecae683b9618e97acaa658c079b9e55fb3
Pavel Perestoronin <eigenein@gmail.com>
2024-12-10 19:28:00 +0100

    OPT: use hashed type adapters ⚡️

---
+++ combadge/_helpers/typing.py
@@ -0,0 +1,17 @@
+from typing import Annotated, Any
+
+from typing_extensions import TypeAliasType, get_args, get_origin
+
+
+def unwrap_type_alias(type_: Any) -> Any:
+    """Extract the inner type, if the given type is a type alias."""
+    if isinstance(type_, TypeAliasType):
+        type_ = type_.__value__
+    return type_
+
+
+def unwrap_annotated(type_: Any) -> Any:
+    """Extract the inner type, if the given type is an `Annotated` form."""
+    if get_origin(type_) is Annotated:
+        type_ = get_args(type_)[0]
+    return type_
